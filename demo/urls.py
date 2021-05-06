cc7de74518db6912c370827550bbaecc52df08e3
Camila Maia <cmaiacd@gmail.com>
2021-05-06 19:57:37 -0300

    DRF Tutorial code

--- demo/urls.py
+++ demo/urls.py
@@ -1,13 +1,11 @@
-from django.http import HttpResponse
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import include, path
 
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("snippets.urls")),
+]
 
-def home(request):
-    return HttpResponse(
-        "<h2>ScanAPI Demo</h2>"
-        "<p>This is an application to help showing how ScanAPI works</p>"
-        "<p>Access the demo API at: <a href=https://demo.scanapi.dev/api>https://demo.scanapi.dev/api</a></p>"
-    )
-
-
-urlpatterns = [path("", home, name="index"), path("api/", include("demo.api.urls"))]
+urlpatterns += [
+    path("api-auth/", include("rest_framework.urls")),
+]
