42ab642e4c075e6dc02a74e48e1b61133d9adf15
Camila Maia <cmaiacd@gmail.com>
2021-05-07 18:26:24 -0300

    Add STATIC_ROOT to settings file

--- demo/settings.py
+++ demo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
