0ce976fbdd961fb63043c617484e1ff1b3eb4b19
Camila Maia <cmaiacd@gmail.com>
2021-05-21 15:25:29 -0300

    Add URLPathVersioning

--- demo/urls.py
+++ demo/urls.py
@@ -1,22 +1,14 @@
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
-from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path("", lambda req: redirect("/api/v1/")),
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
     # DEPRECATED: the old demo api. We are leaving it here only to avoid breaking external code
     # that depends on it.
     path("api/", include("api.urls")),
     path("api/<str:version>/", include("snippets.urls")),
-]
-
-
-## Authentication URLs
-urlpatterns += [
-    path("api-auth/", include("rest_framework.urls")),
-    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
-    path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
