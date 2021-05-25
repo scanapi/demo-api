6a6a5c98de1354a783b193264156d5c1cc7d546c
Camila Maia <cmaiacd@gmail.com>
2021-05-25 16:30:29 -0300

    Add static folder (#33)

--- demo/settings.py
+++ demo/settings.py
@@ -127,12 +127,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = "/static/"
-
 REST_FRAMEWORK = {
     "ALLOWED_VERSIONS": ("v1",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
