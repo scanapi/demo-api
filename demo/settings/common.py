f47a1fdc4aee3b1043600d46506beedf41b93550
Camila Maia <cmaiacd@gmail.com>
2022-08-16 09:19:54 +0200

    Revert "Remove redundant static files from the project #44 (#56)"
    This reverts commit 7dd986433b30929f4e15c08a09fc434a4be332b5.

--- demo/settings/common.py
+++ demo/settings/common.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,11 +130,11 @@ REST_FRAMEWORK = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR.parent.parent / "static"
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 # allauth settings
